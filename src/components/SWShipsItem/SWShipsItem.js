d13fd19233b6cb3bfbcdf2e3c22684059f0d5660
Elena <turonchek09@gmail.com>
2022-02-21 22:41:49 +0200

    bugs fixed

--- src/components/SWShipsItem/SWShipsItem.js
+++ src/components/SWShipsItem/SWShipsItem.js
@@ -1,31 +1,3 @@
-// import React, {PureComponent} from "react";
-// import PropTypes from 'prop-types';
-
-// export class SWShipsItem extends PureComponent{
-    
-//     render(){
-//         const {name, uid, url} = this.props;
-//         return(
-//             <li key={uid}
-//                 className=""
-//                 style={{listStyleType: "none", marginBottom:20,backgroundColor:`rgb(150, 173, 144)`,padding:10}}>
-//                 <h1>{name}</h1>
-//                 <div>{uid}</div>
-//                 <a 
-//                     href={url}
-//                     style={{color:'black',fontWeight:'bold'}}
-//                     >LINK</a>
-//             </li>
-//         );
-//     }
-// }
-
-// SWShipsItem.propTypes = {
-//     name:PropTypes.string.isRequired,
-//     uid:PropTypes.string.isRequired,
-//     url:PropTypes.string.isRequired,
-// }
-
 import React from "react";
 import PropTypes from 'prop-types';
 
--- src/components/SWShipsPage/SWShipsPage.js
+++ src/components/SWShipsPage/SWShipsPage.js
@@ -1,85 +1,3 @@
-// import React, {PureComponent} from "react";
-// import { Pagination } from "../Pagination/Pagination";
-// import { SWShipsList } from "../SWShipsList/SWShipsList";
-
-// export class SWShipsPage extends PureComponent{
-
-//     constructor(props){
-//         super(props);
-
-//         this.state = {
-//             status: 'initial',
-//             error:null,
-//             data:[],
-//             currentPage:1
-//         };
-//     }
-
-
-//     handlePageClick = (data) => {
-//         let currentPage=1+data.selected;
-//         this.setState ({
-//             currentPage,
-//         }, ()=>{
-//             this.fetchData();
-//         })
-//     }
-//     componentDidUpdate(){
-//         console.log(`update`)
-//     }
-
-//     fetchData = () => {
-//         this.setState({
-//             status:'loading',
-//             error:null,
-//             data:[],
-//         });
-
-//         fetch(`https://www.swapi.tech/api/starships?page=${this.state.currentPage}&limit=10`)
-//             .then((res) => {
-//                 return res.json()
-//             })
-//             .then((result) => {
-//                 this.setState({
-//                     status: `success`,
-//                     error: null,
-//                     data:result,
-//                 });
-//             })
-//             .catch((error) => {
-//                 this.setState({
-//                     status: `error`,
-//                     error:error.message,
-//                 });
-//             })
-//     }
-
-//     render(){
-//         console.log(`render`)
-//         const { status, error, data, currentPage } = this.state;
-//         return(
-//             <div>
-//                 <SWShipsList 
-//                     data={data.results}
-//                     status={status}
-//                     error={error}
-//                     />
-//                 {status===`success` && <Pagination 
-//                                             handlePageClick={this.handlePageClick}
-//                                             pageCount={data.total_pages}
-//                                             currentPage={currentPage}
-//                                         />
-//                 }
-//             </div>
-//         );
-//     }
-
-//     componentDidMount() {
-//         console.log(`mount`)
-//         this.fetchData();
-//     }
-// }
-
 import React,{useCallback, useEffect, useState} from "react";
 import { Pagination } from "../Pagination/Pagination";
 import { SWShipsList } from "../SWShipsList/SWShipsList";
@@ -105,7 +23,6 @@ export function SWShipsPage() {
         setError(null);
         setData([]);
         fetch(`https://www.swapi.tech/api/planets?page=${currentPage}&limit=10`)
-        // fetch(`https://www.swapi.tech/api/starships?page=${currentPage}&limit=10`)
             .then((res) => {
                 return res.json();
             })
